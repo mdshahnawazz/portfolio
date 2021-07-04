9d550a418be51cd61bfa9a7ed106c55df25cee1a
mdshahnawazz <faizmerchant65@gmail.com>
2021-07-05 02:35:18 +0530

    first commit

---
+++ src/components/Shop.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Shop=()=> {
+    return (
+        <div className="shop">
+            <div className="container">
+                <div className="shop__text">
+                    <div>
+                        <h1>Free Devlivery With PIZZA OD THE DAY</h1>
+                        <div className="shop__btn">
+                            <a href="#" className="btn btn-smart">SHOP NOW</a>
+                        </div>
+                    </div>
+                </div>
+            </div>           
+        </div>
+    );
+};
+
+export default Shop;
